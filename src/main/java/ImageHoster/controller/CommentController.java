4562fc98382eebcd89941a9c81f8813bd2fcaf30
jay0893 <jaypatel109002@gmail.com>
2018-12-27 12:28:48 +0530

    Initial commit

---
+++ src/main/java/ImageHoster/controller/CommentController.java
@@ -0,0 +1,41 @@
+package ImageHoster.controller;
+
+import ImageHoster.model.Comment;
+import ImageHoster.model.Image;
+import ImageHoster.model.User;
+import ImageHoster.service.CommentService;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Date;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private ImageService imageService;
+
+    @RequestMapping(value = "/image/{imageId}/{imageTitle}/comments",method = RequestMethod.POST)
+
+    public String createComment(@PathVariable("imageId") Integer imageId, @PathVariable("imageTitle") String imageTitle, @RequestParam("comment") String text,Comment comment, HttpSession session) throws IOException {
+        User user = (User) session.getAttribute("loggeduser");
+        Image image = imageService.getImageById(imageId);
+        comment.setUser(user);
+        comment.setText(text);
+        comment.setImage(image);
+        comment.setCreatedDate(new Date());
+        commentService.updateComment(comment);
+        return "redirect:/images/" + imageId;
+    }
+
+}
---
+++ src/main/java/ImageHoster/service/CommentService.java
@@ -0,0 +1,25 @@
+package ImageHoster.service;
+
+import ImageHoster.model.Comment;
+import ImageHoster.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public void updateComment(Comment comment) {
+        commentRepository.updateComment(comment);
+    }
+
+    public List<Comment> getCommentsById(Integer imageId) {
+        List<Comment> results = commentRepository.getCommentsById(imageId);
+        return results;
+    }
+
+}
